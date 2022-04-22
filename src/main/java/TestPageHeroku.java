8d623c8b337b0f5e85f1e371a1998cc1384552f2
juancaarguelles <juan.aguelles@globant.com>
2022-04-22 15:57:16 -0500

    adding test-heroku point

--- src/main/java/TestPageHeroku.java
+++ src/main/java/TestPageHeroku.java
@@ -1,2 +1,14 @@
-public class TestPageHeroku {
+public class TestPageHeroku
+{
+    private String authorName = "By.css body > div > address:nth-child(6) > a:nth-child(1)";
+    private String firstListthirdElement = "By.css body > div > ul > li:nth-child(3)";
+    private String secondListFirstElement = "By.css body > div > menu > li:nth-child(1)";
+    private String thirdListThirdElement = "By.css body > div > dir > li:nth-child(3)";
+    private String bigButton = "By.css #but";
+    private String secondResetButton = "By.css #f01";
+    private String listBoxThirdElement = "By.css #f11 > option:nth-child(3)";
+    private String icelandArea = "By.css body > div > table:nth-child(18) > tbody > tr:nth-child(4) > td:nth-child(3)";
+    private String firstTextHyphernation = "By.css body > div > p:nth-child(23)";
+    private String thirdH3FromHyphernation = "By.css body > div > h3:nth-child(26)";
+
 }
